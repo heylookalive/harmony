4ce364e5f3551064fe54becbe0403a8149172b5f
Alli Price <alli.price@yahoo.co.uk>
2014-01-04 16:14:51 +0000

    Addition of the new thread modal window.

--- assets/js/arcadia.js
+++ assets/js/arcadia.js
@@ -2,4 +2,9 @@
 jQuery(document).ready(function($) {
   // Apply nicer tooltips!
   $('a.help-tooltip').tooltip();
+
+  // Thread settings button.
+  $('a.harmony-entity-settings-toggle').live('click', function(){
+    $(this).next().fadeToggle('fast');
+  });
 });
